db700f44834b2a0cc251d0439576abeadced3eb7
TakumiNakahara <XXXX@hogehoge.com>
2022-05-19 22:40:40 +0900

    how to use props¥

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,47 @@
+import React,{Component} from 'react';
 
+// class App extends Component {
+//   render(){
+//     const greeting = "Hi!This is Takumi!!"
+//     return (
+//       <React.Fragment>
+//         <div className="App">
+//           <header className="App-header">
+//             <h1 className='title'>HelloWorld</h1>
+//           </header>
+//         </div>
+//         <h2>{greeting}</h2>
+//       </React.Fragment>
+      
+//     );
+//   }
+// }
 
-function App() {
+const App = () =>{
+  const profile = [
+    {name: "jhon dow", agr: "28"},
+    {name: "kevin", agr: "20"},
+    {name: "claire", agr: "24"},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>HelloWorld</h1>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <h1 className='title'>登録者名簿</h1>
+      <ul>
+        {
+          profile.map(function(profile , index){
+            return <User name={profile.name}  age={profile.age} key={index}/>
+          })
+        }
+      </ul>
+    </React.Fragment>
+  )
 }
+
+const User = (props) => {
+  return(
+    <li>{props.name}　{props.age}</li>
+  )
+}
+
 
 export default App;
